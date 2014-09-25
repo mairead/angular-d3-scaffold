b8a41d295aa96bdfd761119e282a88154dd34b84
mairead <mdb.buchan@gmail.com>
2014-09-25 15:57:59 +0100

    boilerplate code

---
+++ test/karma.conf.js
@@ -0,0 +1,47 @@
+module.exports = function(config){
+  'use strict';
+
+  config.set({
+
+    autoWatch: true,
+
+    basePath : '../',
+
+    frameworks: ['jasmine'],
+
+    files : [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      'test/spec/**/*.js'
+    ],
+
+    port: 8080,
+    
+
+    browsers : ['PhantomJS'],
+
+    plugins : [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            ],
+
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
+
+  });
+};
