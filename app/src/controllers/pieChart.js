028996142261c96eb8d22e3c5ed5efb9e0406cf0
mairead <mdb.buchan@gmail.com>
2014-10-02 09:14:23 +0100

    interactive pie chart single complete

--- app/src/controllers/pieChart.js
+++ app/src/controllers/pieChart.js
@@ -9,11 +9,28 @@ var PieChart = (function () {
   	var vm = this;
 
   	vm.chartData = [
-  		{"name": "He Man", "strength": 30},
-  		{"name": "She Ra", "strength": 20},
-  		{"name": "Cringer", "strength": 15},
-      {"name": "Swift Wind", "strength": 45}
+  		{"name": "He Man", "strength": 25},
+  		{"name": "She Ra", "strength": 25},
+  		{"name": "Cringer", "strength": 25},
+      {"name": "Swift Wind", "strength": 25}
   	];
+
+    $scope.addCharacter = function(data) {
+      vm.newCharacter = null;
+      console.log("data added", data);
+      var newCharacter = {
+        "name" : data.name,
+        "strength": data.strength
+      }
+      vm.chartData.push(newCharacter);
+    };
+
+    $scope.removeCharacter = function(characterName){
+      //retrieve object from array where name matches item passed
+      vm.chartData = _.filter(vm.chartData, function (character) {
+        return character.name != characterName 
+      });
+    }
 	}
 
 	PieChart.$inject = ['$scope'];
--- app/src/directives/piechart.directive.js
+++ app/src/directives/piechart.directive.js
@@ -22,8 +22,8 @@ var headPie = (function () {
       //using lo-dash here to massage data object
       var data = _.pluck(scope.data, 'strength');
       var color = d3.scale.category10();
-      var width = 300;
-      var height = 300;
+      var width = 250;
+      var height = 250;
       var min = Math.min(width, height);
 
       var pie = d3.layout.pie().sort(null);
@@ -46,8 +46,30 @@ var headPie = (function () {
 
       //udpate pie chart when sliders are moved
       scope.$watch('data',function(){
-        data = _.pluck(scope.data, 'strength')
-        g.selectAll('path').data(pie(data)).attr('d', arc);
+        data = _.pluck(scope.data, 'strength');
+      
+        // console.log(data.length, scope.data)
+        var arcs = g.selectAll('path').data(pie(data))
+        
+        arcs.attr('d', arc)
+          .enter().append('path') //enter here doesn't trigger colour being added?
+          .style('stroke', 'white')
+          .attr('fill', function(d, i){ console.log(i); return color(i) })
+          
+          arcs.exit().remove()
+          
+        
+          //enter should be adding new DOM item to be styled with fill attr
+
+          //need to read up on enter and exit phase. 
+          //to see how these can be used to update data/charts
+
+          //don't want sub selections from enter and exit. 
+          
+          //enter phase adds the colour here, 
+          //but is only taking up the new areas and not the whole circle
+          //because its only rendering part of the
+
       },true)
     }
   }
